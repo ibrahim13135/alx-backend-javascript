135dfda1b948a3e4d04144fc5b40c308d065fd98
Hima <abrahimmahmoud5454@gmail.com>
2024-05-28 01:55:46 +0300

    arrow

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+
+    if (trueOrFalse) {
+        let task = true;
+        let task2 = false;
+    }
+
+    return [task, task2];
+}
+
+
+// [false, true]
+// [false, true]
