621ec74335fa14bd54e9faca7a3518322b56e029
Hima <abrahimmahmoud5454@gmail.com>
2024-05-30 02:00:39 +0300

    my first PENDING

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]).then((results) => {
+        return results.map((result) => ({
+            status: result.status,
+            value: result.value !== undefined ? result.value : result.reason,
+        }));
+    });
+}
