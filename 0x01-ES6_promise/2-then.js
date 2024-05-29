23a8000908b1f70cd5fe381067626cca1f4ff2bd
Hima <abrahimmahmoud5454@gmail.com>
2024-05-30 01:13:18 +0300

    my first all

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
-function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            console.log("Got a response from the API");
-            return {
-                status: 200,
-                body: 'success'
-            };
-        })
-        .catch(() => new Error());
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
-
-
